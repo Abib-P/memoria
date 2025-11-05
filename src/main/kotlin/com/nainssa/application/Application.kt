6111e5769ab9ac6d89b88fe3477b103304c4b9df
nainssa <paulabib17@gmail.com>
2025-11-05 18:00:28 +0100

    remove import *

--- src/main/kotlin/com/nainssa/application/Application.kt
+++ src/main/kotlin/com/nainssa/application/Application.kt
@@ -4,7 +4,8 @@ import com.nainssa.core.coreModule
 import com.nainssa.core.movie.MovieService
 import com.nainssa.infrastructure.controller.movie.moviesRoute
 import com.nainssa.infrastructure.repository.repositoryModule
-import io.ktor.server.application.*
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
 import io.ktor.server.routing.routing
 import org.koin.ktor.ext.inject
 import org.koin.ktor.plugin.Koin
--- src/main/kotlin/com/nainssa/infrastructure/controller/movie/MovieController.kt
+++ src/main/kotlin/com/nainssa/infrastructure/controller/movie/MovieController.kt
@@ -1,8 +1,10 @@
 package com.nainssa.infrastructure.controller.movie
 
 import com.nainssa.core.movie.MovieService
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+import io.ktor.server.routing.route
 
 fun Route.moviesRoute(movieService: MovieService) {
     route("/movies") {
